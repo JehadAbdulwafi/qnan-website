c83d76bb4e8e0cd27b36b8de00db3dcfbc29f99a
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-12 19:59:26 +0200

    move globals.css to styles dir

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
-import "../public/globals.css"
+import "@/styles/globals.css"
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component { ...pageProps } />
+  return <Component {...pageProps} />
 }
 
 export default appWithTranslation(App)
