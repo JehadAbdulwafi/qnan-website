e49b93c6573a6e2c49f5e906c6fe0087067dbbe1
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-14 18:44:40 +0200

    add sections to home page

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import { getStaticPaths, makeStaticProps } from '../../lib/getStatic'
 import Layout from '@/components/layout'
+import AboutUs from '@/components/home/about-us';
+import Hero from '@/components/home/hero';
+import Download from '@/components/home/download';
+import Featured from '@/components/home/featured';
+import Testimonials from '@/components/home/testimonials';
+import Benefits from '@/components/home/benefits';
 
 const Homepage = () => {
   const [isClient, setIsClient] = useState(false);
@@ -15,7 +21,12 @@ const Homepage = () => {
 
   return (
     <Layout>
-      <div>Jehad</div>
+      <Hero />
+      <AboutUs />
+      <Featured />
+      <Testimonials />
+      <Benefits />
+      <Download />
     </Layout>
   )
 }
