686fc67717772ac389610225edfe93db06e2ef9d
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-15 22:07:25 +0200

    add sanity token

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,8 +1,10 @@
 import { createClient } from "next-sanity";
+import { token } from "./token";
 
 export const client = createClient({
   projectId: "h9isjcos",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
+  token: token,
 });
---
+++ lib/token.ts
@@ -0,0 +1,5 @@
+export const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+
+if (!token) {
+  throw new Error("Missing NEXT_PUBLIC_SANITY_TOKEN");
+}
