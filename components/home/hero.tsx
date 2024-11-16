c1a01de8d6ba73c91edf55c06c8b147039b32627
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-16 17:11:37 +0200

    remove bg image for hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -9,31 +9,26 @@ export default function Hero() {
     <section
       className="h-[580px] min-h-[560px] w-full relative"
     >
-      <Image
-        src={require("../../public/images/bg.png")}
-        alt="iphone"
-        className="absolute top-0 left-0 w-full object-center object-cover h-[580px] min-h-[560px]"
-
-      />
       <div className="relative px-6 flex flex-col h-full w-full pt-14">
         <div className="mx-auto flex-1">
           <div className="text-center">
-            <h1 className="text-balance text-5xl font-[700] tracking-tight text-[#bf9f60] sm:text-5xl">
+            <h1 className="text-balance text-3xl font-[700] tracking-tight text-sky-500 sm:text-5xl">
               {t('banner-title')}
             </h1>
-            <p className="mt-2 text-white text-pretty text-lg font-normal sm:text-[16px]">
+            <p className="mt-2 text-pretty text-lg font-normal sm:text-[16px]">
               {t('banner-desc')}
             </p>
           </div>
         </div>
-        <div className="max-w-[720px] relative flex-1 mx-auto flex-grow">
+        <div className="w-full relative h-full mx-auto">
           <Image
             src={require("../../public/images/iphone-3.png")}
             alt="iphone"
-            className="w-full object-contain relative top-0"
+            fill
+            className="w-full object-contain scale-125"
           />
         </div>
-      </div>
+        </div>
     </section>
   )
 }
