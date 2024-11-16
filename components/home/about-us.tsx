efdd979fb089991a0505e0cf7f493b16d2cd7395
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-16 22:25:00 +0200

    add banner bg

--- components/home/about-us.tsx
+++ components/home/about-us.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 export default function AboutUs() {
   const { t } = useTranslation('common');
   return (
-    <section className='pt-24 py-10'>
+    <section className='py-10 sm:pt-32'>
       <div className='container text-center'>
         <div className='section-title'>
           <h2>{t('about_us-title')}</h2>
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,30 +6,28 @@ export default function Hero() {
   const { t } = useTranslation('common');
 
   return (
-    <section
-      className="h-[580px] min-h-[560px] w-full relative"
+    <div
+      className="h-[590px] sm:h-[720px] min-h-[560px] w-full relative"
     >
-      <div className="relative px-6 flex flex-col h-full w-full pt-14">
-        <div className="mx-auto flex-1">
-          <div className="text-center">
-            <h1 className="text-balance text-3xl font-[700] tracking-tight text-sky-500 sm:text-5xl">
-              {t('banner-title')}
-            </h1>
-            <p className="mt-2 text-pretty text-lg font-normal sm:text-[16px]">
-              {t('banner-desc')}
-            </p>
-          </div>
+      <div className="relative flex flex-1 flex-col h-full w-full">
+        <div className="w-full relative h-[80%]">
+          <Image
+            src={require("../../public/images/banner.webp")}
+            alt="iphone"
+            fill
+            className="w-full h-full object-cover relative"
+          />
         </div>
-        <div className="w-full relative h-full mx-auto">
+        <div className="w-full absolute h-[75%] -bottom-24 sm:-bottom-40">
           <Image
             src={require("../../public/images/iphone-3.png")}
             alt="iphone"
             fill
-            className="w-full object-contain"
+            className="object-contain"
           />
         </div>
-        </div>
-    </section>
+      </div>
+    </div>
   )
 }
 
